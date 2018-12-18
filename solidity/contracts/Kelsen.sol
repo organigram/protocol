7f846f793ef5fe9b35f17e1a8d944d7dfe0acdcd
LIEUTAUD Henri <henri.lieutaud@groupe-vyv.fr>
2018-12-18 15:54:35 +0100

    Updated compiler version to homogenize

--- solidity/contracts/Kelsen.sol
+++ solidity/contracts/Kelsen.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 
+
 /// @title Standard organ contract
 
 contract Kelsen {
--- solidity/contracts/procedures/simpleAdminsAndMasterNominationProcedure.sol
+++ solidity/contracts/procedures/simpleAdminsAndMasterNominationProcedure.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.11;
+pragma solidity >=0.4.22 <0.6.0;
+
 
 // Standard contract for a presidential election procedure
 

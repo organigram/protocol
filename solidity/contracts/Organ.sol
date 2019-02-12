4029259f0875c41d84a2b1a6f4641580f0aef7ca
l-Henri <henri.lieutaud@groupe-vyv.fr>
2019-02-12 09:48:18 +0100

    Using library structure variables

--- solidity/contracts/Organ.sol
+++ solidity/contracts/Organ.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.4.22 <0.6.0;
 
 
 import "./Kelsen.sol";
+import "./libraries/organLibrary.sol";
 
 
 contract Organ is Kelsen{
@@ -17,33 +18,9 @@ contract Organ is Kelsen{
     bool public isAnOrgan = true;
     bool public isAProcedure = false;
     int public kelsenVersionNumber = 1;
-    string public organName;
-
-    struct Master {
-        string name; // Master name
-        bool canAdd;  // if true, master can add admins
-        bool canDelete;  // if true, master can delete admins
-        uint rankInMasterList; // Rank in dynamic array masterList
-
-    }
-
-    struct Admin {
-        string name; // Admin name
-        bool canAdd;  // if true, Admin can add norms
-        bool canDelete;  // if true, Admin can delete norms
-        bool canSpend;
-        bool canDeposit;
-        uint rankInAdminList; // Rank in dynamic array adminList
-    }
-
-    struct Norm {
-        string name; // Master name
-        address normAddress; // Address if norm is a member or a contract
-        bytes32 ipfsHash; // ID of proposal on IPFS
-        uint8 hash_function;
-        uint8 size;
-    }
-
+    
+    // Storing organ infos
+    organLibrary.OrganInfo organInfos;
     // Events
     // Organ management events
     event changeOrganName(address _from, string _newName);
@@ -66,8 +43,8 @@ contract Organ is Kelsen{
 
     // This declares two state variable that
     // stores a `Master` and a 'admin' struct for each possible address.
-    mapping(address => Master) public masters;
-    mapping(address => Admin) public admins;
+    //mapping(address => organLibrary.Master) public masters;
+    //mapping(address => organLibrary.Admin) public admins;
 
     // One mapping to signal adresses that are included in norms
     mapping(address => bool) public isAddressInNorms;
@@ -75,33 +52,33 @@ contract Organ is Kelsen{
     mapping(address => uint) public addressPositionInNorms;
 
     // This declares two dynamically-sized array of `Admin` and 'Master' structs for easy referencing.
-    address[] public masterList;
-    address[] public adminList;
+    //address[] public masterList;
+    //address[] public adminList;
 
     // A dynamically-sized array of `Norm` structs.
-    Norm[] public norms;
+    //organLibrary.Norm[] public norms;
 
-
+    //string public organName;
     // Keeping track of active norms
-    uint256 public activeNormNumber;
+    //uint256 public activeNormNumber;
 
 
     constructor(string _name) public {
 
         // Initializing with deployer as master
-        masters[msg.sender].canAdd = true;
-        masters[msg.sender].canDelete = true;
-        masters[msg.sender].name = 'Original Master';
-        masterList.push(msg.sender);
-        organName = _name;
+        organInfos.masters[msg.sender].canAdd = true;
+        organInfos.masters[msg.sender].canDelete = true;
+        organInfos.masters[msg.sender].name = 'Original Master';
+        organInfos.masterList.push(msg.sender);
+        organInfos.organName = _name;
         // Initializing with deployer as admin
         // admins[msg.sender].canAdd = true;
         // admins[msg.sender].canDelete = true;
         // admins[msg.sender].name = 'Original Master';
         // adminList.push(msg.sender);
         // Initializing first norms to avoid errors when deleting norms
-        Norm memory initNorm;
-        norms.push(initNorm);
+        organLibrary.Norm memory initNorm;
+        organInfos.norms.push(initNorm);
         kelsenVersionNumber = 1;
 
     }
@@ -109,19 +86,19 @@ contract Organ is Kelsen{
 
     function setName(string _name) public {
         // Check sender is allowed
-        require((masters[msg.sender].canAdd) && (masters[msg.sender].canDelete));
-        organName = _name;
+        require((organInfos.masters[msg.sender].canAdd) && (organInfos.masters[msg.sender].canDelete));
+        organInfos.organName = _name;
         emit changeOrganName(msg.sender, _name);
     }
         // Money managing function
     function () public payable {
-        require(admins[msg.sender].canDeposit);
+        require(organInfos.admins[msg.sender].canDeposit);
         emit receiveMoney(msg.sender, msg.value);
 
 
     }
     function payout(address _to, uint _value) public {
-        require(admins[msg.sender].canSpend);
+        require(organInfos.admins[msg.sender].canSpend);
         _to.transfer(_value);
         emit spendMoney(msg.sender, _to, _value);
 
@@ -131,27 +108,27 @@ contract Organ is Kelsen{
 
     function addMaster(address _newMasterAddress, bool _canAdd, bool _canDelete, string _name) public{
         // Check that the sender is allowed
-        require((masters[msg.sender].canAdd));
+        require((organInfos.masters[msg.sender].canAdd));
         // Check new master is not already a master
-        require((!masters[_newMasterAddress].canAdd) && (!masters[_newMasterAddress].canDelete));
+        require((!organInfos.masters[_newMasterAddress].canAdd) && (!organInfos.masters[_newMasterAddress].canDelete));
 
         // Check new master has at least one permission activated
         require(_canAdd || _canDelete);
 
         // Adding master to master list and retrieving position
-        masters[_newMasterAddress].rankInMasterList = masterList.push(_newMasterAddress) - 1;
+        organInfos.masters[_newMasterAddress].rankInMasterList = organInfos.masterList.push(_newMasterAddress) - 1;
 
         // Creating master privileges
-        masters[_newMasterAddress].canAdd = _canAdd;
-        masters[_newMasterAddress].canDelete = _canDelete;
-        masters[_newMasterAddress].name = _name;
+        organInfos.masters[_newMasterAddress].canAdd = _canAdd;
+        organInfos.masters[_newMasterAddress].canDelete = _canDelete;
+        organInfos.masters[_newMasterAddress].name = _name;
         emit addMasterEvent(msg.sender, _newMasterAddress, _canAdd, _canDelete, _name);
 
     }
 
     function replaceMaster(address _masterToRemove, address _masterToAdd, bool _canAdd, bool _canDelete, string _name) public {
         // Check sender is allowed
-        require((masters[msg.sender].canAdd) && (masters[msg.sender].canDelete));
+        require((organInfos.masters[msg.sender].canAdd) && (organInfos.masters[msg.sender].canDelete));
         // Check new master has at least one permission activated
         require(_canAdd || _canDelete);
 
@@ -173,9 +150,9 @@ contract Organ is Kelsen{
             emit addMasterEvent(msg.sender, _masterToAdd, _canAdd, _canDelete, _name);
 
             //Modifying permissions
-            masters[_masterToRemove].canAdd = _canAdd;
-            masters[_masterToRemove].canDelete = _canDelete;
-            masters[_masterToRemove].name = _name;
+            organInfos.masters[_masterToRemove].canAdd = _canAdd;
+            organInfos.masters[_masterToRemove].canDelete = _canDelete;
+            organInfos.masters[_masterToRemove].name = _name;
 
         }
 
@@ -183,13 +160,13 @@ contract Organ is Kelsen{
     }
     function remMaster(address _masterToRemove) public {
         // Check sender is allowed
-        require((masters[msg.sender].canDelete));
+        require((organInfos.masters[msg.sender].canDelete));
         // Check affected account is a master
-        require((masters[_masterToRemove].canDelete) || (masters[_masterToRemove].canAdd) );
+        require((organInfos.masters[_masterToRemove].canDelete) || (organInfos.masters[_masterToRemove].canAdd) );
         // Deleting entry in masterList
-        delete masterList[masters[_masterToRemove].rankInMasterList];
+        delete organInfos.masterList[organInfos.masters[_masterToRemove].rankInMasterList];
         // Deleting master privileges
-        delete masters[_masterToRemove];
+        delete organInfos.masters[_masterToRemove];
         emit remMasterEvent(msg.sender, _masterToRemove);
     }
 
@@ -197,29 +174,29 @@ contract Organ is Kelsen{
 
     function addAdmin(address _newAdminAddress, bool _canAdd, bool _canDelete, bool _canDeposit, bool _canSpend, string _name) public {
         // Check the sender is allowed
-        require((masters[msg.sender].canAdd));
+        require((organInfos.masters[msg.sender].canAdd));
         // Check new admin is not already an admin
-        require((!admins[_newAdminAddress].canAdd) && (!admins[_newAdminAddress].canDelete) && (!admins[_newAdminAddress].canDeposit) && (!admins[_newAdminAddress].canSpend));
+        require((!organInfos.admins[_newAdminAddress].canAdd) && (!organInfos.admins[_newAdminAddress].canDelete) && (!organInfos.admins[_newAdminAddress].canDeposit) && (!organInfos.admins[_newAdminAddress].canSpend));
 
         // Check new admin has at least one permission activated
         require(_canAdd || _canDelete || _canDeposit || _canSpend);
 
         // Adding admin to admin list and retrieving position
-        admins[_newAdminAddress].rankInAdminList = adminList.push(_newAdminAddress) - 1;
+        organInfos.admins[_newAdminAddress].rankInAdminList = organInfos.adminList.push(_newAdminAddress) - 1;
 
         // Creating master privileges
-        admins[_newAdminAddress].canAdd = _canAdd;
-        admins[_newAdminAddress].canDelete = _canDelete;
-        admins[_newAdminAddress].canDeposit = _canDeposit;
-        admins[_newAdminAddress].canSpend = _canSpend;
-        admins[_newAdminAddress].name = _name;
+        organInfos.admins[_newAdminAddress].canAdd = _canAdd;
+        organInfos.admins[_newAdminAddress].canDelete = _canDelete;
+        organInfos.admins[_newAdminAddress].canDeposit = _canDeposit;
+        organInfos.admins[_newAdminAddress].canSpend = _canSpend;
+        organInfos.admins[_newAdminAddress].name = _name;
         emit addAdminEvent(msg.sender, _newAdminAddress,  _canAdd,  _canDelete,  _canDeposit,  _canSpend,  _name);
    
     }
 
     function replaceAdmin(address _adminToRemove, address _adminToAdd, bool _canAdd, bool _canDelete, bool _canDeposit, bool _canSpend, string _name) public {
         // Check sender is allowed
-        require((masters[msg.sender].canAdd) && (masters[msg.sender].canDelete));
+        require((organInfos.masters[msg.sender].canAdd) && (organInfos.masters[msg.sender].canDelete));
         // Check new admin has at least one permission activated
         require(_canAdd || _canDelete || _canDeposit || _canSpend);
         
@@ -230,13 +207,13 @@ contract Organ is Kelsen{
 
     function remAdmin(address _adminToRemove) public {
         // Check sender is allowed
-        require((masters[msg.sender].canDelete));
+        require((organInfos.masters[msg.sender].canDelete));
         // Check affected account is admin
-        require((admins[_adminToRemove].canDelete) || (admins[_adminToRemove].canAdd) );
+        require((organInfos.admins[_adminToRemove].canDelete) || (organInfos.admins[_adminToRemove].canAdd) );
         // Deleting entry in adminList
-        delete adminList[admins[_adminToRemove].rankInAdminList];
+        delete organInfos.adminList[organInfos.admins[_adminToRemove].rankInAdminList];
         // Deleting admin privileges
-        delete admins[_adminToRemove];
+        delete organInfos.admins[_adminToRemove];
 
         emit remAdminEvent(msg.sender, _adminToRemove);
 
@@ -247,13 +224,13 @@ contract Organ is Kelsen{
     function addNorm (address _normAddress, string _name, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size) public  returns (uint _normPosition)
     {
         // Check sender is allowed
-        require(admins[msg.sender].canAdd);
+        require(organInfos.admins[msg.sender].canAdd);
 
         // If the norm has an address, we check that the address has not been used before.
         if (_normAddress != 0x0000) { require(!isAddressInNorms[_normAddress]);}
 
         // Adding the norm
-        norms.push(Norm({
+        organInfos.norms.push(organLibrary.Norm({
                 name: _name,
                 normAddress: _normAddress,
                 ipfsHash: _ipfsHash,
@@ -261,9 +238,9 @@ contract Organ is Kelsen{
                 size: _size
             }));
         // Registering norm position relative to its address
-        addressPositionInNorms[_normAddress] = norms.length -1;
+        addressPositionInNorms[_normAddress] = organInfos.norms.length -1;
         // Incrementing active norm number and total norm number trackers
-        activeNormNumber = activeNormNumber + 1;
+        organInfos.activeNormNumber += 1;
         emit addNormEvent(msg.sender, _normAddress,  _name,  _ipfsHash,  _hash_function,  _size);
 
         // Registering the address as active
@@ -273,14 +250,14 @@ contract Organ is Kelsen{
 
     function replaceNorm (uint _normNumber, address _normAddress, string _name, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size) public
     {
-        require((admins[msg.sender].canDelete) && (admins[msg.sender].canAdd));
+        require((organInfos.admins[msg.sender].canDelete) && (organInfos.admins[msg.sender].canAdd));
         if (_normAddress != 0x0000) { require(!isAddressInNorms[_normAddress]);}
-        isAddressInNorms[norms[_normNumber].normAddress] = false;
-        addressPositionInNorms[norms[_normNumber].normAddress] = 0;
-        emit remNormEvent(msg.sender, norms[_normNumber].normAddress, norms[_normNumber].name, norms[_normNumber].ipfsHash,  norms[_normNumber].hash_function,  norms[_normNumber].size);
+        isAddressInNorms[organInfos.norms[_normNumber].normAddress] = false;
+        addressPositionInNorms[organInfos.norms[_normNumber].normAddress] = 0;
+        emit remNormEvent(msg.sender, organInfos.norms[_normNumber].normAddress, organInfos.norms[_normNumber].name, organInfos.norms[_normNumber].ipfsHash,  organInfos.norms[_normNumber].hash_function,  organInfos.norms[_normNumber].size);
 
-        delete norms[_normNumber];
-        norms[_normNumber] = Norm({
+        delete organInfos.norms[_normNumber];
+        organInfos.norms[_normNumber] = organLibrary.Norm({
                 name: _name,
                 normAddress: _normAddress,
                 ipfsHash: _ipfsHash,
@@ -298,17 +275,17 @@ contract Organ is Kelsen{
         // Check sender is allowed:
         // - Sender is admin
         // - Norm number is trying to delete himself
-        require(admins[msg.sender].canDelete || (isAddressInNorms[msg.sender] && msg.sender == norms[_normNumber].normAddress));
+        require(organInfos.admins[msg.sender].canDelete || (isAddressInNorms[msg.sender] && msg.sender == organInfos.norms[_normNumber].normAddress));
         // Deleting norm position from addressPositionInNorms
-        delete addressPositionInNorms[norms[_normNumber].normAddress];
+        delete addressPositionInNorms[organInfos.norms[_normNumber].normAddress];
         // Marking address as deactivated from isAddressInNorms
-        isAddressInNorms[norms[_normNumber].normAddress] = false;
+        isAddressInNorms[organInfos.norms[_normNumber].normAddress] = false;
         // Logging event
-        emit remNormEvent(msg.sender, norms[_normNumber].normAddress, norms[_normNumber].name, norms[_normNumber].ipfsHash,  norms[_normNumber].hash_function,  norms[_normNumber].size);
+        emit remNormEvent(msg.sender, organInfos.norms[_normNumber].normAddress, organInfos.norms[_normNumber].name, organInfos.norms[_normNumber].ipfsHash,  organInfos.norms[_normNumber].hash_function,  organInfos.norms[_normNumber].size);
 
         // Removing norm from norms
-        delete norms[_normNumber];
-        activeNormNumber = activeNormNumber - 1;
+        delete organInfos.norms[_normNumber];
+        organInfos.activeNormNumber -= 1;
 
 
     }
@@ -316,39 +293,39 @@ contract Organ is Kelsen{
     //////////////////////// Functions to communicate with other contracts
     // Checking individual addresses
     function isMaster (address _adressToCheck) public view returns (bool canAdd, bool canDelete) {
-        return (masters[_adressToCheck].canAdd, masters[_adressToCheck].canDelete);
+        return (organInfos.masters[_adressToCheck].canAdd, organInfos.masters[_adressToCheck].canDelete);
     }
     function isAdmin (address _adressToCheck) public view returns (bool canAdd, bool canDelete) {
-        return (admins[_adressToCheck].canAdd, admins[_adressToCheck].canDelete);
+        return (organInfos.admins[_adressToCheck].canAdd, organInfos.admins[_adressToCheck].canDelete);
     }
     function isNorm (address _adressToCheck) public view returns (bool isAddressInNorm) {
         return isAddressInNorms[_adressToCheck];
     }
     function isMoneyManager(address _adressToCheck) public view returns (bool canDeposit, bool canSpend){
-        return (admins[_adressToCheck].canDeposit, admins[_adressToCheck].canSpend);
+        return (organInfos.admins[_adressToCheck].canDeposit, organInfos.admins[_adressToCheck].canSpend);
     }
     // Retrieve contract state info
     // Number of active norms (voter pool size for voter registering, for example)
     function getActiveNormNumber() public view returns (uint _activeNormNumber){
-        return activeNormNumber;
+        return organInfos.activeNormNumber;
     }
     // Size of norm array, to list elements
     function getNormListSize() public view returns (uint normArraySize){
-        return norms.length;
+        return organInfos.norms.length;
     }
     function getAddressPositionInNorm(address _addressToCheck) public view returns (uint addressInNormPosition){
         return addressPositionInNorms[_addressToCheck];
     }
     function getSingleNorm(uint _desiredNormPosition) public view returns (string name, address normAddress, bytes32 ipfsHash, uint8 hash_function, uint8 size){
-        return (norms[_desiredNormPosition].name, norms[_desiredNormPosition].normAddress, norms[_desiredNormPosition].ipfsHash, norms[_desiredNormPosition].hash_function, norms[_desiredNormPosition].size);
+        return (organInfos.norms[_desiredNormPosition].name, organInfos.norms[_desiredNormPosition].normAddress, organInfos.norms[_desiredNormPosition].ipfsHash, organInfos.norms[_desiredNormPosition].hash_function, organInfos.norms[_desiredNormPosition].size);
     }
 
     // Retrieve lists of adresses
     function getMasterList() public view returns (address[] _masterList){
-        return masterList;
+        return organInfos.masterList;
     }
     function getAdminList() public view returns (address[] _adminList){
-        return adminList;
+        return organInfos.adminList;
     }
     function getKelsenVersion() public view returns(bool _isAnOrgan, bool _isAProcedure, int _versionNumber)
     {
--- solidity/contracts/libraries/organLibrary.sol
+++ solidity/contracts/libraries/organLibrary.sol
@@ -11,5 +11,39 @@ This library is used to hold all the logic to manage a simple organ.
 **/
 library organLibrary {
   
+    struct Master {
+        string name; // Master name
+        bool canAdd;  // if true, master can add admins
+        bool canDelete;  // if true, master can delete admins
+        uint rankInMasterList; // Rank in dynamic array masterList
+    }
+
+    struct Admin {
+        string name; // Admin name
+        bool canAdd;  // if true, Admin can add norms
+        bool canDelete;  // if true, Admin can delete norms
+        bool canSpend;
+        bool canDeposit;
+        uint rankInAdminList; // Rank in dynamic array adminList
+    }
+
+    struct Norm {
+        string name; // Master name
+        address normAddress; // Address if norm is a member or a contract
+        bytes32 ipfsHash; // ID of proposal on IPFS
+        uint8 hash_function;
+        uint8 size;
+    }
+
+    struct OrganInfo {
+        string organName;
+        uint256 activeNormNumber;
+        address[] masterList;
+        address[] adminList;
+        Norm[] norms;
+        mapping(address => Master) masters;
+        mapping(address => Admin) admins;
+    }
+
 
 }

10041ad651f01d412db4bf80ac8bcb00719d7b8d
l-Henri <henri.lieutaud@groupe-vyv.fr>
2019-02-20 11:50:17 +0100

    Delegated manyToOne to library

--- solidity/contracts/libraries/votingLibrary.sol
+++ solidity/contracts/libraries/votingLibrary.sol
@@ -37,6 +37,8 @@ library votingLibrary {
         uint reelectionMaximum;
         // Minimum participation to validate election
         uint quorumSize;
+        // A mapping of candidacy status and number of candidacies per member
+        mapping(address => uint) cumulatedCandidacies;
         // // Is blank vote accepted
         // bool neutralVoteAccepted;
     }
@@ -58,12 +60,19 @@ library votingLibrary {
         uint candidacyEndDate;
         uint electionEndDate;
         bool wasEnded;
-        bool wasEnforced;
         uint totalVoteCount;
         address[] candidateList;
         mapping(address => bool) hasUserVoted;
         mapping(address => Candidacy) candidacies;
     }
+
+    // Events
+    event ballotCreationEvent(address _from, bytes32 _name, uint _startDate, uint _candidacyEndDate, uint _endDate, uint _ballotNumber);
+    event presentCandidacyEvent(uint _ballotNumber, address _candidateAddress, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size);
+    event votedOnElectionEvent(address _from, uint _ballotNumber);
+    event ballotWasCounted(uint _ballotNumber, address[] _candidateList, address _winningCandidate, uint _totalVoteCount);
+    event ballotResultException(uint _ballotNumber);
+    event ballotWasEnforced(address _winningCandidate, uint _ballotNumber);
 
     function initElectionParameters(RecurringElectionInfo storage self, uint _ballotFrequency, uint _ballotDuration, uint _quorumSize, uint _reelectionMaximum, uint _candidacyDuration)
     public
@@ -74,6 +83,191 @@ library votingLibrary {
         self.quorumSize = _quorumSize;
         self.reelectionMaximum = _reelectionMaximum;
         self.candidacyDuration = _candidacyDuration;
+    }
+
+    function createRecurrentBallotManyToOne(RecurringElectionInfo storage self, ElectionBallot[] storage ballots, bytes32 _ballotName) 
+    public 
+    returns (uint ballotNumber)
+    {
+        // Checking that election date has passed
+        require (now > self.nextElectionDate);
+        // Checking if previous ballot was counted
+        if (ballots.length > 0) 
+        {
+            require(ballots[ballots.length - 1].wasEnded);
+        }
+
+        ElectionBallot memory newBallot;
+        newBallot.name = _ballotName;
+        newBallot.startDate = now;
+        newBallot.candidacyEndDate = now + self.candidacyDuration;
+        newBallot.electionEndDate = now + self.candidacyDuration+self.ballotDuration;
+        newBallot.wasEnded = false;
+        ballots.push(newBallot);
+
+        ballotNumber = ballots.length - 1;
+
+        // openBallotList.push(ballotNumber);
+        // currentBallot = ballotNumber;
+        self.nextElectionDate = now + self.ballotFrequency;
+
+        // Ballot creation event
+        emit ballotCreationEvent(msg.sender, newBallot.name, newBallot.startDate, newBallot.candidacyEndDate, newBallot.electionEndDate, ballotNumber);
+
+        return ballotNumber;
+    }
+
+    function presentCandidacyLib(RecurringElectionInfo storage self, ElectionBallot storage ballot, uint _ballotNumber, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size) 
+    public 
+    {
+        // Check that the ballot is still active
+        require(!ballot.wasEnded);
+
+        // Check that the ballot candidacy period is still open
+        require(ballot.candidacyEndDate > now);
+
+        // Check that sender is not over the mandate limit
+        require(self.cumulatedCandidacies[msg.sender] < self.reelectionMaximum);
+
+        // Check if the candidate is already candidate
+        require(ballot.candidacies[msg.sender].candidateAddress != msg.sender);
+
+        ballot.candidateList.push(msg.sender);
+
+        ballot.candidacies[msg.sender].candidateAddress = msg.sender;
+
+        ballot.candidacies[msg.sender].ipfsHash = _ipfsHash;
+        ballot.candidacies[msg.sender].hash_function = _hash_function;
+        ballot.candidacies[msg.sender].size = _size;
+        ballot.candidacies[msg.sender].voteNumber = 0;
+         // Candidacy event is turned off for now
+        emit presentCandidacyEvent(_ballotNumber, msg.sender, _ipfsHash, _hash_function, _size);
+    }
+
+    function voteManyToOne(ElectionBallot storage self, uint _ballotNumber, address _candidateAddress) 
+    public 
+    {        
+        // Check if voter already votred
+        require(!self.hasUserVoted[msg.sender]);
+
+        // Check if vote is still active
+        require(!self.wasEnded);
+
+        // Check if candidacy period is over
+        require(self.candidacyEndDate < now);
+
+        // Check if voting period ended
+        require(self.electionEndDate > now);
+
+        // Check if candidate for whom we voted for is declared
+        if(self.candidacies[_candidateAddress].candidateAddress != 0x0000)
+        {self.candidacies[_candidateAddress].voteNumber += 1;}
+        else
+            // If candidate does not exist, this is a neutral vote
+        {self.candidacies[0x0000].voteNumber += 1;}
+
+        self.hasUserVoted[msg.sender] = true;
+        
+        self.totalVoteCount += 1;
+
+        // Event
+        emit votedOnElectionEvent(msg.sender, _ballotNumber);
+    }
+
+    function countManyToOne(RecurringElectionInfo storage self, ElectionBallot storage ballot, address _votersOrgan, uint _ballotNumber) 
+    public 
+    returns (address nextPresident)
+    {
+        // We check if the vote was already closed
+        require(!ballot.wasEnded);
+
+        // Checking that the vote can be closed
+        require(ballot.electionEndDate < now);
+
+        // Checking that there was enough participation
+        if ((ballot.candidateList.length == 0) || ballot.totalVoteCount == 0)
+        {
+                ballot.wasEnded = true;
+                emit ballotResultException(_ballotNumber);
+                self.nextElectionDate = now -1;
+                return 0x0000;
+        }
+
+        // Checking if the election is still valid
+        if (now > ballot.startDate + self.ballotFrequency)
+        {
+            emit ballotResultException(_ballotNumber);
+            ballot.wasEnded = true;  
+            return 0x0000;                
+        }
+
+
+
+
+        // ############## Going through candidates to check the vote count
+        uint winningVoteCount = 0;
+        bool isADraw = false;
+        bool quorumIsObtained = false;
+
+        Organ voterRegistryOrgan = Organ(_votersOrgan);
+
+        // Check if quorum is obtained. We avoiding divisions here, since Solidity is not good to calculate divisions
+        ( ,uint voterNumber) = voterRegistryOrgan.organInfos();
+        if (ballot.totalVoteCount*100 >= self.quorumSize*voterNumber)
+        {
+            quorumIsObtained = true;
+        }
+
+        delete voterRegistryOrgan;
+
+        // Going through candidates list
+        for (uint p = 0; p < ballot.candidateList.length; p++) 
+        {
+            address _candidateAddress = ballot.candidateList[p];
+            if (ballot.candidacies[_candidateAddress].voteNumber > winningVoteCount) 
+            {
+                winningVoteCount = ballot.candidacies[_candidateAddress].voteNumber ;
+                nextPresident = ballot.candidateList[p];
+                isADraw = false;
+            }
+
+            else if (ballot.candidacies[_candidateAddress].voteNumber == winningVoteCount)
+            {
+                isADraw = true;
+            }
+        }
+
+        // ############## Updating ballot values if vote concluded
+        ballot.wasEnded = true;
+
+        if (!isADraw && quorumIsObtained)
+            // The ballot completed succesfully
+        {
+            emit ballotWasCounted(_ballotNumber, ballot.candidateList, nextPresident, ballot.totalVoteCount);
+        }
+
+        else // The ballot did not conclude correctly. We reboot the election process.
+        {
+            nextPresident = 0x0000;
+            emit ballotResultException(_ballotNumber);
+            self.nextElectionDate = now -1;
+            ballot.wasEnded = true;
+        }
+
+        return nextPresident;    
+    }
+
+    function givePowerToNewPresident(ElectionBallot storage ballot, address _newPresident, address _currentPresident, address _presidentialOrganAddress, uint _ballotNumber)
+    public
+    {
+        Organ presidentialOrgan = Organ(_presidentialOrganAddress);
+        presidentialOrgan.addNorm(_newPresident, ballot.candidacies[_newPresident].ipfsHash, ballot.candidacies[_newPresident].hash_function, ballot.candidacies[_newPresident].size);
+        if (_ballotNumber > 0)
+        {
+            presidentialOrgan.remNorm(presidentialOrgan.getAddressPositionInNorm(_currentPresident));
+        }
+        
+        emit ballotWasEnforced(_newPresident, _ballotNumber);
     }
 
 }
--- solidity/contracts/procedures/cyclicalManyToOneElectionProcedure.sol
+++ solidity/contracts/procedures/cyclicalManyToOneElectionProcedure.sol
@@ -32,26 +32,11 @@ contract cyclicalManyToOneElectionProcedure is Procedure
     // current President address
     address public currentPresident;
 
-    struct BallotResults
-        {
-            address winningCandidate;
-            uint totalVoteCount;
-        }
-
     // A dynamically-sized array of `Ballot` structs.
     votingLibrary.ElectionBallot[] public ballots;
-    BallotResults[] public ballotResults;
-
-    // A mapping of candidacy status and number of candidacies per member
-    mapping(address => uint) public cumulatedCandidacies;
 
     // Events
-    event ballotCreationEvent(address _from, bytes32 _name, uint _startDate, uint _candidacyEndDate, uint _endDate, uint _ballotNumber);
-    event presentCandidacyEvent(uint _ballotNumber, address _candidateAddress, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size);
-    event votedOnElectionEvent(address _from, uint _ballotNumber);
-    event ballotWasCounted(uint _ballotNumber, address[] _candidateList, address _winningCandidate, uint _totalVoteCount);
-    event ballotResultException(uint _ballotNumber, bool _wasRebooted);
-    event ballotWasEnforced(address _winningCandidate, uint _ballotNumber);
+    event ballotResultException(uint _ballotNumber);
 
     constructor(address _referenceOrganContract, address _affectedOrganContract, uint _ballotFrequency, uint _ballotDuration, uint _quorumSize, uint _reelectionMaximum, bytes32 _name) 
     public 
@@ -67,31 +52,7 @@ contract cyclicalManyToOneElectionProcedure is Procedure
     returns (uint ballotNumber)
     {
 
-        // Checking that election date has passed
-        require (now > electionParameters.nextElectionDate);
-        // Checking if previous ballot was counted
-        if (ballots.length > 0) {
-            require(ballots[ballots.length - 1].wasEnded);
-        }
-
-        votingLibrary.ElectionBallot memory newBallot;
-        newBallot.name = _ballotName;
-        newBallot.startDate = now;
-        newBallot.candidacyEndDate = now + electionParameters.candidacyDuration;
-        newBallot.electionEndDate = now + electionParameters.candidacyDuration+electionParameters.ballotDuration;
-        newBallot.wasEnded = false;
-        ballots.push(newBallot);
-
-        ballotNumber = ballots.length - 1;
-
-        BallotResults memory newBallotResults;
-        ballotResults.push(newBallotResults);
-        // openBallotList.push(ballotNumber);
-        // currentBallot = ballotNumber;
-        electionParameters.nextElectionDate = now + electionParameters.ballotFrequency;
-
-        // Ballot creation event
-        emit ballotCreationEvent(msg.sender, newBallot.name, newBallot.startDate, newBallot.candidacyEndDate, newBallot.electionEndDate, ballotNumber);
+        return electionParameters.createRecurrentBallotManyToOne(ballots, _ballotName);
     }
 
     function presentCandidacy(uint _ballotNumber, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size) 
@@ -100,30 +61,7 @@ contract cyclicalManyToOneElectionProcedure is Procedure
 
         // Check the candidate is a member of the reference organ
         linkedOrgans.firstOrganAddress.isAllowed();
-
-
-        // Check that the ballot is still active
-        require(!ballots[_ballotNumber].wasEnded);
-
-        // Check that the ballot candidacy period is still open
-        require(ballots[_ballotNumber].candidacyEndDate > now);
-
-        // Check that sender is not over the mandate limit
-        require(cumulatedCandidacies[msg.sender] < electionParameters.reelectionMaximum);
-
-        // Check if the candidate is already candidate
-        require(ballots[_ballotNumber].candidacies[msg.sender].candidateAddress != msg.sender);
-
-        ballots[_ballotNumber].candidateList.push(msg.sender);
-
-        ballots[_ballotNumber].candidacies[msg.sender].candidateAddress = msg.sender;
-
-        ballots[_ballotNumber].candidacies[msg.sender].ipfsHash = _ipfsHash;
-        ballots[_ballotNumber].candidacies[msg.sender].hash_function = _hash_function;
-        ballots[_ballotNumber].candidacies[msg.sender].size = _size;
-        ballots[_ballotNumber].candidacies[msg.sender].voteNumber = 0;
-         // Candidacy event is turned off for now
-        emit presentCandidacyEvent(_ballotNumber, msg.sender, _ipfsHash, _hash_function, _size);
+        electionParameters.presentCandidacyLib(ballots[_ballotNumber], _ballotNumber, _ipfsHash, _hash_function, _size);
     }
 
 
@@ -131,162 +69,38 @@ contract cyclicalManyToOneElectionProcedure is Procedure
     function vote(uint _ballotNumber, address _candidateAddress) 
     public 
     {
-
         linkedOrgans.firstOrganAddress.isAllowed();
-        
-        // Check if voter already votred
-        require(!ballots[_ballotNumber].hasUserVoted[msg.sender]);
-
-        // Check if vote is still active
-        require(!ballots[_ballotNumber].wasEnded);
-
-        // Check if candidacy period is over
-        require(ballots[_ballotNumber].candidacyEndDate < now);
-
-        // Check if voting period ended
-        require(ballots[_ballotNumber].electionEndDate > now);
-
-        // Check if candidate for whom we voted for is declared
-        if(ballots[_ballotNumber].candidacies[_candidateAddress].candidateAddress != 0x0000)
-        {ballots[_ballotNumber].candidacies[_candidateAddress].voteNumber += 1;}
-        else
-            // If candidate does not exist, this is a neutral vote
-        {ballots[_ballotNumber].candidacies[0x0000].voteNumber += 1;}
-
-        ballots[_ballotNumber].hasUserVoted[msg.sender] = true;
-        
-        ballots[_ballotNumber].totalVoteCount += 1;
-
-        // Event
-        emit votedOnElectionEvent(msg.sender, _ballotNumber);
-                        
-
+        ballots[_ballotNumber].voteManyToOne(_ballotNumber, _candidateAddress);
     }
 
     // The vote is finished and we close it. This triggers the outcome of the vote.
 
     function endBallot(uint _ballotNumber) 
     public 
-    returns (uint winningCandidate)
+    returns (address electionWinner)
     {
-        // We check if the vote was already closed
-        require(!ballots[_ballotNumber].wasEnded);
+        electionWinner = electionParameters.countManyToOne(ballots[_ballotNumber], linkedOrgans.firstOrganAddress, _ballotNumber);   
 
-        // Checking that the vote can be closed
-        require(ballots[_ballotNumber].electionEndDate < now);
-
-        // Checking that the vote can be closed
-        if ((ballots[_ballotNumber].candidateList.length == 0) || ballots[_ballotNumber].totalVoteCount == 0)
+        if (electionWinner == 0x0000)
         {
-                ballots[_ballotNumber].wasEnforced = true;
-                ballots[_ballotNumber].wasEnded = true;
-                emit ballotResultException(_ballotNumber, true);
-                electionParameters.nextElectionDate = now -1;
-                createBallot(ballots[_ballotNumber].name);
-                return 0;
-        }
+            emit ballotResultException(_ballotNumber); 
+        }  
 
-
-        // ############## Going through candidates to check the vote count
-        uint winningVoteCount = 0;
-        bool isADraw = false;
-        bool quorumIsObtained = false;
-
-        Organ voterRegistryOrgan = Organ(linkedOrgans.firstOrganAddress);
-
-        // Check if quorum is obtained. We avoiding divisions here, since Solidity is not good to calculate divisions
-        ( ,uint voterNumber) = voterRegistryOrgan.organInfos();
-        if (ballots[_ballotNumber].totalVoteCount*100 >= electionParameters.quorumSize*voterNumber)
-        {quorumIsObtained = true;}
-
-        // Going through candidates list
-        for (uint p = 0; p < ballots[_ballotNumber].candidateList.length; p++) {
-            address _candidateAddress = ballots[_ballotNumber].candidateList[p];
-            if (ballots[_ballotNumber].candidacies[_candidateAddress].voteNumber > winningVoteCount) {
-                winningVoteCount = ballots[_ballotNumber].candidacies[_candidateAddress].voteNumber ;
-                winningCandidate = p;
-                isADraw = false;
-            }
-            else if (ballots[_ballotNumber].candidacies[_candidateAddress].voteNumber == winningVoteCount){
-                isADraw = true;
-            }
-
-        }
-
-        // ############## Updating ballot values if vote concluded
-        ballots[_ballotNumber].wasEnded = true;
-
-        if (!isADraw && quorumIsObtained)
-            // The ballot completed succesfully
+        else  
         {
+            electionParameters.cumulatedCandidacies[electionWinner] += 1;
 
-
-        ballotResults[_ballotNumber].winningCandidate = ballots[_ballotNumber].candidateList[winningCandidate];
-        emit ballotWasCounted(_ballotNumber, ballots[_ballotNumber].candidateList, ballotResults[_ballotNumber].winningCandidate, ballots[_ballotNumber].totalVoteCount);
-
-            }
-            else
-                // The ballot did not conclude correctly. We reboot the election process.
+            if (electionWinner != currentPresident)
             {
-                ballots[_ballotNumber].wasEnforced = true;
-                emit ballotResultException(_ballotNumber, true);
-                electionParameters.nextElectionDate = now -1;
-                ballots[_ballotNumber].wasEnded = true;
-                createBallot(ballots[_ballotNumber].name);
+                ballots[_ballotNumber].givePowerToNewPresident(electionWinner, currentPresident, linkedOrgans.secondOrganAddress, _ballotNumber);
+                // Modifying procedure variable to count new president
+                currentPresident = electionWinner;
             }
-                    
-    }
-    
-    function enforceBallot(uint _ballotNumber) public {
-        // Checking if ballot was already enforced
-        require(!ballots[_ballotNumber].wasEnforced );
-        // Checking the ballot was closed
-        require(ballots[_ballotNumber].wasEnded);
-        // Checking that the enforcing date is not later than the end of his supposed mandate
-        if (now > ballots[_ballotNumber].startDate + electionParameters.ballotFrequency)
-        {
-            ballots[_ballotNumber].wasEnforced = true;
-            // TODO add event to log this  
-            emit ballotResultException(_ballotNumber, false);                  
-            return;
         }
-
-        // We initiate the Organ interface to add a presidential norm
-
-        Organ presidentialOrgan = Organ(linkedOrgans.secondOrganAddress);
-        address newPresidentAddress = ballotResults[_ballotNumber].winningCandidate;
-
-        // Adding new president first
-            // function addNorm (string _name, address _normAdress, bytes32 _ipfsHash, uint8 _hash_function, uint8 _size) public  returns (bool success);
-        if (newPresidentAddress != currentPresident)
-            {
-            votingLibrary.Candidacy memory newPresident = ballots[_ballotNumber].candidacies[newPresidentAddress];
-            presidentialOrgan.addNorm(newPresidentAddress, newPresident.ipfsHash, newPresident.hash_function, newPresident.size );
-
             
-
-            // Removing former president, only if a former election was conducted.
-            if (ballots.length > 0){
-            presidentialOrgan.remNorm(presidentialOrgan.getAddressPositionInNorm(currentPresident));
-                }
-
-
-            // Modifying procedure variable to count new president
-            currentPresident = newPresidentAddress;
-
-            }
-        cumulatedCandidacies[newPresidentAddress] += 1;
-        ballots[_ballotNumber].wasEnforced = true;
-        // delete openBallotList[_ballotNumber];
-        // currentBallot = 0;
-
-        // Adjusting mandate duration
-        // nextElectionDate = now + ballotFrequency - candidacyDuration - ballotDuration;
-        emit ballotWasEnforced( newPresidentAddress, _ballotNumber);
-         }
-        //////////////////////// Functions to communicate with other contracts
-    
-
+        return electionWinner;          
+    }
+        
     // ######### Functions to retrieve procedure infos
 
     function getCandidateList(uint _ballotNumber) 

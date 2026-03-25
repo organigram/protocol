7037aee8bc28e1097fe3367f14272c1f4ee29727
louis-md <louis@margot-duclot.fr>
2026-03-25 23:35:38 +0700

    Bump pragma solidity to ^0.8.24

--- script/DeployProtocol.s.sol
+++ script/DeployProtocol.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import {CoreLibrary} from "../contracts/libraries/CoreLibrary.sol";
 import {Organ} from "../contracts/Organ.sol";
